1bee96835ad13bc6f422ccc814abf45836b07141
leriahes <valeriya-shevchenko-2002@mail.ru>
2024-11-24 00:05:53 +0700

    Raise NotFound error when Car not found

--- src/cars/views.py
+++ src/cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.exceptions import NotFound
 
 from .models import *
 from .serializers import *
@@ -13,4 +14,8 @@ class CommentListCreateView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         car_id = self.kwargs.get('id')
+        try:
+            Car.objects.get(id=car_id)
+        except Car.DoesNotExist:
+            raise NotFound(detail="No Car matches the given query.")
         return Comment.objects.filter(car=car_id)
