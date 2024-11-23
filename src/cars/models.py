c914828e3eccf1932c4355edae8c7bff2ac45d2d
leriahes <valeriya-shevchenko-2002@mail.ru>
2024-11-23 20:32:18 +0700

    Add __str__ methods into models

--- src/cars/models.py
+++ src/cars/models.py
@@ -10,8 +10,14 @@ class Car(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cars')
 
+    def __str__(self):
+        return f"{self.make} {self.model} ({self.year}) - {self.owner.username}"
+
 class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Комментарий пользователя {self.author.username} от {self.created_at} [{self.car}]"
