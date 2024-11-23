66ef43864fee0dc5765478e11febc618ba201ec1
leriahes <valeriya-shevchenko-2002@mail.ru>
2024-11-24 00:14:47 +0700

    Set permissions

--- src/cars/views.py
+++ src/cars/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import *
 from .serializers import *
@@ -8,9 +9,11 @@ from .serializers import *
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CommentListCreateView(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         car_id = self.kwargs.get('id')
