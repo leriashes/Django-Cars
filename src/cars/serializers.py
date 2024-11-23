b8a3402cb7152b48b26cd7c21ba6195042990b97
leriahes <valeriya-shevchenko-2002@mail.ru>
2024-11-24 00:17:59 +0700

    Match new objects to current user

--- src/cars/serializers.py
+++ src/cars/serializers.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 from .models import *
 
 class CarSerializer(serializers.ModelSerializer):
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Car
         fields = '__all__'
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Comment
         fields = '__all__'
