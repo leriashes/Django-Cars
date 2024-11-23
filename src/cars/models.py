299d8e42459215bec6a3a745008b8149a78548d5
leriahes <valeriya-shevchenko-2002@mail.ru>
2024-11-23 20:11:39 +0700

    Made year and description fields in model Car optional

--- src/cars/models.py
+++ src/cars/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    year = models.PositiveIntegerField()
-    description = models.TextField()
+    year = models.PositiveIntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cars')
